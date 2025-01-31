d6bf07c7f250eeffc01bb58160ba59db0b612f20
fabricio-Viana <fabricio.viana.2301@gmail.com>
2025-01-31 01:01:48 -0300

    Operadores aritimeticos

---
+++ 03.Comentarios.py
@@ -0,0 +1,15 @@
+# minhas variaveis
+salario = 4740.44
+despesas = 2345.56
+
+# hoje estou aprendendo python 
+# esta sendo legal isso se eu 
+# puder mesmo aprender 
+
+"""
+a ideia é quanto vai sobra no final dio mes 
+"""
+
+#Print('fim')
+print(salario - despesas)
+print('FIM DE VERDADE')
---
+++ 05.DesafioOperadores.py
@@ -0,0 +1,7 @@
+salario = 3450.45
+despesas = 2456.2
+
+porcentagemDeGastos = despesas/ salario * 100
+
+
+print(f'{porcentagemDeGastos}%') 
