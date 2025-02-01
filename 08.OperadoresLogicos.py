728ed50a06fe1913ef4deb85ebd26d5b93a95a3a
fabricio-Viana <fabricio.viana.2301@gmail.com>
2025-02-01 01:21:04 -0300

    Operadores logicos + DESAFIO

---
+++ 08.OperadoresLogicos.py
@@ -0,0 +1,9 @@
+print(True or False)
+print(7 != 3 and 3 > 3)
+
+print(True != False) # xor é um ou esclusivo
+print(False != False)
+
+print(not True)
+print(not False)
+# print(!True) isto não funciona em python
---
+++ 09.Desafio.py
@@ -0,0 +1,15 @@
+terca = False
+
+quinta  = True
+
+if(terca == True and quinta == True):
+    print('O homen foi ao shipping e comprou uma tv de 50 polegadas')
+    print('e tomaram sorvete')
+if(terca == True or quinta == True):
+    print('O homen foi ao shipping e comprou uma tv de 32 polegadas')
+    print('e tomaram sorvete')
+
+if(terca == False and quinta == False):
+    print('não fomos ao shopping')
+    print('não tomamos sorvete')
+    print('saude +1') 
